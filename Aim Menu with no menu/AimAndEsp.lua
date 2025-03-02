e7de3199d1ca2bab640b8889edb8d16ddd76961f
Syfer-eng (Banana Man) <reviewedpuppy93@gmail.com>
2025-03-02 12:27:39 -0600

    Update AimAndEsp.lua

--- Aim Menu with no menu/AimAndEsp.lua
+++ Aim Menu with no menu/AimAndEsp.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SYFER-eng/Knite_menu.lua/refs/heads/main/Cornerbox.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SYFER-eng/Knite_menu.lua/refs/heads/main/Aim%20Menu%20with%20no%20menu/Aim-Code.lua",true))()
